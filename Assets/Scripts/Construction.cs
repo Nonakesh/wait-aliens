d59ea1f9631e130f30dbfb73e01435effeb97ed8
MrMyth-M <marcolpayer@gmail.com>
2019-08-10 15:29:20 +0200

    Construction (finally) and basic GameUI

---
+++ Assets/Scripts/Construction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+public class Construction : MonoBehaviour
+{
+    public float BuildingTime;
+
+    public Health FinalBuilding;
+
+    private Health _health;
+    public Health Health => _health ?? (_health = GetComponent<Health>());
+
+    private float buildRate;
+    
+    private void Start()
+    {
+        Health.CurrentHealth = 1;
+        Health.MaxHealth = FinalBuilding.MaxHealth;
+        
+        buildRate = FinalBuilding.MaxHealth / BuildingTime;
+    }
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        Health.CurrentHealth += buildRate * Time.deltaTime;
+        
+        if (Health.CurrentHealth >= FinalBuilding.MaxHealth)
+        {
+            var t = transform;
+            Instantiate(FinalBuilding, t.position, t.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+
+    public Button PauseButton;
+    public Sprite ClockSprite;
+    public Sprite PlaySprite;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        if (PauseButton.image.sprite == ClockSprite)
+        {
+            PauseButton.image.sprite = PlaySprite;
+        }
+        else
+        {
+            PauseButton.image.sprite = ClockSprite;
+        }
+        
+        Debug.Log("Slowing down time and pausing...");
+    }
+}
