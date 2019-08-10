f801fb548b4c10a74d9253b1baa305f0896e3aa5
MrMyth-M <marcolpayer@gmail.com>
2019-08-10 12:47:14 +0200

    Construction, Health, etc

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,9 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    public float MaxHealth;
+    public float CurrentHealth;
+    public float RegenerationPerSecond;
+    public float RegenerationTick = 1;
+    public ResourceDrop[] Drops;
+
+    public Transform DeathEffect;
+    
+    private float timeSinceLastRegeneration;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,46 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Regenerate();
+        CheckDeathState();
+    }
+    
+    public void TakeDamage(float amount)
+    {
+        CurrentHealth -= amount;
+    }
+    
+    private void DropResources()
+    {
+        foreach (var drop in Drops)
+        {
+            ResourceManager.AddResource(drop.Type, drop.Amount);
+        }
+    }
+
+    private void Regenerate()
+    {
+        timeSinceLastRegeneration += Time.deltaTime;
+        if (timeSinceLastRegeneration >= RegenerationTick)
+        {
+            timeSinceLastRegeneration = 0;
+            CurrentHealth += RegenerationPerSecond;
+        }
+    }
+
+    private void CheckDeathState()
+    {
+        if (CurrentHealth <= 0)
+        {
+            DropResources();
+
+            if (DeathEffect != null)
+            {
+                var t = transform;
+                Instantiate(DeathEffect, t.position, t.rotation);
+            }
+            
+            Destroy(gameObject);
+        }
     }
 }
