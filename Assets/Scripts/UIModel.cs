8c754005e3acd0ccfd33c61d9261f1ff8326f54d
Simon Stix <simon.stix@gmail.com>
2019-08-11 12:56:47 +0200

    Fix UI 3d model icons

---
+++ Assets/Scripts/UIModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIModel : MonoBehaviour
+{
+    public float Horizontal = 0.7f;
+    public float Vertical = 0.7f;
+    
+    public float rotationSpeed = 15f;
+    public Transform model;
+
+    void Update()
+    {
+        var t = transform as RectTransform;
+
+        var corners = new Vector3[4];
+        t.GetWorldCorners(corners);
+
+        var invHorizonal = 1 - Horizontal;
+        var invVertical = 1 - Vertical;
+
+        model.position = (Horizontal * Vertical * corners[0] +
+                          Horizontal * invVertical * corners[1] +
+                          invHorizonal * invVertical * corners[2] +
+                          invHorizonal * Vertical * corners[3]);
+
+        model.Rotate(rotationSpeed * Time.unscaledDeltaTime * Vector3.up, Space.Self);
+        model.gameObject.SetActive(true);
+    }
+}
