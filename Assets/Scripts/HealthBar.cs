a5865cbd215669c1ba14b7aba9334d5e8d8aed62
Bernhard Reinsprecht <Bernhard.Reinsprecht@catalysts.cc>
2019-08-11 13:19:26 +0200

    added dynamic health color

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,11 +8,14 @@ public class HealthBar : MonoBehaviour
 {
     [Range(0, 1)]
     public float Health;
+    public Gradient Gradient;
+    public Image Fill;
 
     public Slider Slider;
 
     private void Update()
     {
         Slider.value = Health;
+        Fill.color = Gradient.Evaluate(Health);
     }
 }
