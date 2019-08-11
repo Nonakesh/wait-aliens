40881a9876259465beaeda7ea4bced64682395e8
MrMyth-M <marcolpayer@gmail.com>
2019-08-11 11:52:09 +0200

    better UI, MainMenu, etc

---
+++ Assets/Scripts/UIRotationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRotationManager : MonoBehaviour
+{
+    
+    public float rotationSpeed = 15f;
+    public Transform myTransform;
+    
+    void Update()
+    {
+        myTransform.Rotate(rotationSpeed * Time.deltaTime * Vector3.up, Space.Self);
+        myTransform.gameObject.SetActive(true);
+    }
+}
